386be22a8605139e812e09b406e393967c08f360
mikenguyen69 <nguyenduchung0609@gmail.com>
2018-03-20 23:15:09 +0800

    Add project files.

---
+++ ObservablerApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObservablerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
